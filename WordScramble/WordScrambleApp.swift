e7bc8ce2f482a57f71b9063184c1a4b998c2742d
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-07-15 19:21:53 +0100

    initial commit
    raw code added

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Shun Le Yi Mon on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
